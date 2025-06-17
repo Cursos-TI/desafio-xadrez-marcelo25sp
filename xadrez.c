183bc007c04aa8f3c3900be2e1b7ba36040c15a6
Marcelo Nascimento <105380422+marcelo25sp@users.noreply.github.com>
2025-06-17 23:47:25 +0000

    implementando a lógica do "bispo" no código

--- xadrez.c
+++ xadrez.c
@@ -6,12 +6,22 @@
 int main() {
 
     int casasTorre = 5;
+    int casasBispo = 5;
 
     printf("Movimento da torre\n");
-
-    // implantando uma estrutura de repetição para a movimentação de cinco casas a direita da torre
+    // implementando uma estrutura de repetição para a movimentação de cinco casas a direita da torre
     for(int i = 1;i <= casasTorre;i++){
         printf("Direita\n");
+    }
+
+    printf("---------------------------------\n");
+
+    printf("Movimento do bispo\n");
+    // implementando uma estrutura de repetição de cinco casas "cima direita" do bispo
+    int i=1;
+    while(i <= casasBispo){
+        printf("Cima Direita\n");
+        i++;
     }
 
     return 0;
